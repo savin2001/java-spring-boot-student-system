f058e1b1c4a3ec33faa6cecdac08fa05b70183af
Savin Osuka <osukasavin2001@gmail.com>
2023-01-26 11:24:28 +0300

    Project initialization

---
+++ src/main/java/firsttutorial/com/example/firsttutorial/FirstTutorialApplication.java
@@ -0,0 +1,13 @@
+package firsttutorial.com.example.firsttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/firsttutorial/com/example/firsttutorial/FirstTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package firsttutorial.com.example.firsttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
