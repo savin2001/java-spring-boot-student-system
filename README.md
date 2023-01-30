3203e814108b1115f19505b79edae70a9e782480
Savin Osuka <osukasavin2001@gmail.com>
2023-01-30 08:07:29 +0300

    Still getting error 404 when server runs.

---
+++ README.md
@@ -0,0 +1,7 @@
+## Java Spring Boot Student Application project
+
+This is a demo project to help me understand making a REST-full back-end application using JSB. 
+
+It covers the following sections:
+1. Part One
+2. 
--- src/main/java/firsttutorial/com/example/firsttutorial/FirstTutorialApplication.java
+++ src/main/java/firsttutorial/com/example/firsttutorial/FirstTutorialApplication.java
@@ -1,13 +1,35 @@
 package firsttutorial.com.example.firsttutorial;
 
+import firsttutorial.com.example.firsttutorial.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class FirstTutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstTutorialApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Savin",
+						"test@mele.com",
+						LocalDate.of(2005, Month.JULY, 7),
+						22
+
+				)
+		);
 	}
 
 }
