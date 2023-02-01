88da3fdc0802bdb7608a68f5905050a4b9b5261e
Savin Osuka <osukasavin2001@gmail.com>
2023-02-01 07:38:49 +0300

    Created student Table in the DB.

--- src/main/java/firsttutorial/com/example/firsttutorial/student/Student.java
+++ src/main/java/firsttutorial/com/example/firsttutorial/student/Student.java
@@ -1,8 +1,23 @@
 package firsttutorial.com.example.firsttutorial.student;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+// Creating table in DB
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private  String email;
