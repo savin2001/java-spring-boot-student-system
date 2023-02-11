1bc3dc94f7b2fc0168ee188f7caedc9f90c12056
Savin Osuka <osukasavin2001@gmail.com>
2023-02-11 21:41:25 +0300

    Checking if student exist and adding new to db.

--- src/main/java/firsttutorial/com/example/firsttutorial/student/StudentRepository.java
+++ src/main/java/firsttutorial/com/example/firsttutorial/student/StudentRepository.java
@@ -1,8 +1,17 @@
 package firsttutorial.com.example.firsttutorial.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    // Check if the email exists in the DB
+    // Method 1
+    @Query(" SELECT s FROM Student s WHERE s.email =?1")
+    // Method 2
+    Optional<Student> findStudentByEmail(String email);
 }
--- src/main/java/firsttutorial/com/example/firsttutorial/student/StudentService.java
+++ src/main/java/firsttutorial/com/example/firsttutorial/student/StudentService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -31,6 +32,13 @@ public class StudentService {
 
     // Adding new students to the db
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        // Here you can do complex validation such as whether email is valid or not, e.t.c
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+
+        // Checking if email exists
+        if (studentOptional.isPresent()){
+            throw new IllegalStateException("Email already taken");
+        }
+        studentRepository.save(student);
     }
 }
